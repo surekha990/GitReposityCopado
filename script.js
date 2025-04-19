96cb979a8e92284df9f0a7227fa44ad236622a8e
surekha990 <surekhabandaru13@gmail.com>
2025-04-19 16:12:52 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function beforesubmit()
-{
-    let outputdate=document.querySelector(".outputdate");
-    let inputdate=document.querySelector(".inputdate");
-    console.log("inputdate.value",inputdate.value);
-    let formattedDate=new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value=formattedDate;
-}
